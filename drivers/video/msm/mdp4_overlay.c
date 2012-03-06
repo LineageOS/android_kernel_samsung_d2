bc2ca46eee3c3a1a6ecfefc17023ccf3afef7c23
Adrian Salido-Moreno <adrianm@codeaurora.org>
2012-03-06 13:49:19 -0800

    msm_fb: display: perform data flush for each data path separately
    Performing data flush on stage commit causes some frame updates to be
    missed waiting for stage commit, this can lead to old data being
    overwritten while it's being displayed causing visible corruption.
    Change flush to happen on each frame update to avoid corruptions.
    
    CRs-Fixed: 341597
    Change-Id: If91800068d2bfd5a2e43fe3e2a96c457e173763d
    Signed-off-by: Adrian Salido-Moreno <adrianm@codeaurora.org>

--- drivers/video/msm/mdp4_overlay.c
+++ drivers/video/msm/mdp4_overlay.c
@@ -2938,6 +2938,7 @@ int mdp4_overlay_play(struct fb_info *info, struct msmfb_overlay_data *req)
 		/* primary interface */
 		ctrl->mixer0_played++;
 		if (ctrl->panel_mode & MDP4_PANEL_LCDC) {
+			mdp4_overlay_reg_flush(pipe, 0);
 			if (!mfd->use_ov0_blt)
 				mdp4_overlay_update_blt_mode(mfd);
 			mdp4_overlay_lcdc_start();
@@ -2948,6 +2949,7 @@ int mdp4_overlay_play(struct fb_info *info, struct msmfb_overlay_data *req)
 		}
 #ifdef CONFIG_FB_MSM_MIPI_DSI
 		else if (ctrl->panel_mode & MDP4_PANEL_DSI_VIDEO) {
+			mdp4_overlay_reg_flush(pipe, 0);
 			if (!mfd->use_ov0_blt)
 				mdp4_overlay_update_blt_mode(mfd);
 			mdp4_overlay_dsi_video_start();
