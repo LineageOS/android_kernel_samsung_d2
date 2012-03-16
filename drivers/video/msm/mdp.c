3af1af594e9dd2f2797dcfd494e9c6061fbf1d92
Ravishangar Kalyanam <rkalya@codeaurora.org>
2012-03-15 18:17:54 -0700

    msm: display: Add device pointer with clk_get for retrieving clocks
    clk_get requires device pointer to return appropriate clocks
    for display subsystem. Rename the names of display related clocks
    in the lookup table based on functionality.
    
    CRs-Fixed: 336925
    Change-Id: I784bc4e650d779d1422a9ff1b9a75e36d669e551
    Signed-off-by: Ravishangar Kalyanam <rkalya@codeaurora.org>
    
    Conflicts:
    
    	arch/arm/mach-msm/clock-8960.c
    	arch/arm/mach-msm/clock-8x60.c
    	arch/arm/mach-msm/clock-pcom-lookup.c
    	drivers/video/msm/lvds.c
    	drivers/video/msm/mdp.c
    	drivers/video/msm/mipi_dsi.c
    	drivers/video/msm/msm_dss_io_8960.c

--- drivers/video/msm/mdp.c
+++ drivers/video/msm/mdp.c
@@ -50,6 +50,7 @@ static struct clk *mdp_pclk;
 static struct clk *mdp_lut_clk;
 int mdp_rev;
 
+static struct platform_device *mdp_init_pdev;
 static struct regulator *footswitch;
 static unsigned int mdp_footswitch_on;
 
@@ -2115,7 +2116,8 @@ unsigned long mdp_perf_level2clk_rate(uint32 perf_level)
 	return clk_rate;
 }
 
-static int mdp_irq_clk_setup(char cont_splashScreen)
+static int mdp_irq_clk_setup(struct platform_device *pdev,
+	char cont_splashScreen)
 {
 	int ret;
 
@@ -2138,7 +2140,7 @@ static int mdp_irq_clk_setup(char cont_splashScreen)
 		mdp_footswitch_on = 1;
 	}
 
-	mdp_clk = clk_get(NULL, "mdp_clk");
+	mdp_clk = clk_get(&pdev->dev, "core_clk");
 	if (IS_ERR(mdp_clk)) {
 		ret = PTR_ERR(mdp_clk);
 		printk(KERN_ERR "can't get mdp_clk error:%d!\n", ret);
@@ -2146,12 +2148,12 @@ static int mdp_irq_clk_setup(char cont_splashScreen)
 		return ret;
 	}
 
-	mdp_pclk = clk_get(NULL, "mdp_pclk");
+	mdp_pclk = clk_get(&pdev->dev, "iface_clk");
 	if (IS_ERR(mdp_pclk))
 		mdp_pclk = NULL;
 
-	if (mdp_rev == MDP_REV_42) {
-		mdp_lut_clk = clk_get(NULL, "lut_mdp");
+	if (mdp_rev >= MDP_REV_42) {
+		mdp_lut_clk = clk_get(&pdev->dev, "lut_clk");
 		if (IS_ERR(mdp_lut_clk)) {
 			ret = PTR_ERR(mdp_lut_clk);
 			pr_err("can't get mdp_clk error:%d!\n", ret);
@@ -2202,7 +2204,7 @@ static int mdp_probe(struct platform_device *pdev)
 	static int contSplash_update_done;
 
 	if ((pdev->id == 0) && (pdev->num_resources > 0)) {
-
+		mdp_init_pdev = pdev;
 		mdp_pdata = pdev->dev.platform_data;
 
 		size =  resource_size(&pdev->resource[0]);
@@ -2222,7 +2224,7 @@ static int mdp_probe(struct platform_device *pdev)
 
 		mdp_rev = mdp_pdata->mdp_rev;
 
-		rc = mdp_irq_clk_setup(mdp_pdata->cont_splash_enabled);
+		rc = mdp_irq_clk_setup(pdev, mdp_pdata->cont_splash_enabled);
 
 		if (rc)
 			return rc;
@@ -2386,7 +2388,7 @@ static int mdp_probe(struct platform_device *pdev)
 
 		mdp4_display_intf_sel(if_no, intf);
 #endif
-		mdp_config_vsync(mfd);
+		mdp_config_vsync(mdp_init_pdev, mfd);
 		break;
 
 #ifdef CONFIG_FB_MSM_MIPI_DSI
@@ -2463,7 +2465,7 @@ static int mdp_probe(struct platform_device *pdev)
 			goto mdp_probe_err;
 		}
 #endif
-		mdp_config_vsync(mfd);
+		mdp_config_vsync(mdp_init_pdev, mfd);
 		break;
 #endif
 
