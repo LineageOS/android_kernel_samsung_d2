a0f159079c724a0be61a545aa22b7e77b0fdee50
Mitchel Humpherys <mitchelh@codeaurora.org>
2012-12-04 16:32:05 -0800

    gpu: ion: move msm-specific ioctls to custom_ioctl
    To avoid collisions with future ioctls that may be added to ion,
    msm-specific ioctls should use the `custom_ioctl' interface provided
    by struct ion_device, as originally intended.
    
    Change-Id: Ic36f1ff37ce79926482942f70d466acf95c9c170
    Signed-off-by: Mitchel Humpherys <mitchelh@codeaurora.org>

--- drivers/gpu/ion/ion.c
+++ drivers/gpu/ion/ion.c
@@ -1533,64 +1533,9 @@ static long ion_ioctl(struct file *filp, unsigned int cmd, unsigned long arg)
 	case ION_IOC_CLEAN_CACHES:
 	case ION_IOC_INV_CACHES:
 	case ION_IOC_CLEAN_INV_CACHES:
-	{
-		struct ion_flush_data data;
-		unsigned long start, end;
-		struct ion_handle *handle = NULL;
-		int ret;
-
-		if (copy_from_user(&data, (void __user *)arg,
-				sizeof(struct ion_flush_data)))
-			return -EFAULT;
-
-		start = (unsigned long) data.vaddr;
-		end = (unsigned long) data.vaddr + data.length;
-
-		if (check_vaddr_bounds(start, end)) {
-			pr_err("%s: virtual address %p is out of bounds\n",
-				__func__, data.vaddr);
-			return -EINVAL;
-		}
-
-		if (!data.handle) {
-			handle = ion_import_fd(client, data.fd);
-			if (IS_ERR_OR_NULL(handle)) {
-				pr_info("%s: Could not import handle: %d\n",
-					__func__, (int)handle);
-				return -EINVAL;
-			}
-		}
-
-		ret = ion_do_cache_op(client,
-					data.handle ? data.handle : handle,
-					data.vaddr, data.offset, data.length,
-					cmd);
-
-		if (!data.handle)
-			ion_free(client, handle);
-
-		if (ret < 0)
-			return ret;
-		break;
-
-	}
 	case ION_IOC_GET_FLAGS:
-	{
-		struct ion_flag_data data;
-		int ret;
-		if (copy_from_user(&data, (void __user *)arg,
-				   sizeof(struct ion_flag_data)))
-			return -EFAULT;
-
-		ret = ion_handle_get_flags(client, data.handle, &data.flags);
-		if (ret < 0)
-			return ret;
-		if (copy_to_user((void __user *)arg, &data,
-				 sizeof(struct ion_flag_data)))
-			return -EFAULT;
-		break;
-	}
-	default:
+            return client->dev->custom_ioctl(client, cmd, arg);
+       default:
 		return -ENOTTY;
 	}
 	return 0;
