d6399da9b69b04cc963f7c430bb54da3a5f8bb3d
AproSamurai <AproSamurai@gmail.com>
2013-08-28 13:12:53 -0700

    Touchscreen optimizations
    - Increase TS bandwidth
    - Merge i2c transactions
    - Clean up i2c-qup.c
    
    Based off of commit 96c9624abb195fe67c7f02130703d33e034c74c5
    
    Located at: https://github.com/DecimalMan/dkp
    
    Change-Id: I568ac6445afab7e342456e1d963f0328696f911d

--- drivers/input/input-mt.c
+++ drivers/input/input-mt.c
@@ -93,11 +93,11 @@ void input_mt_report_slot_state(struct input_dev *dev,
 
 	mt = &dev->mt[dev->slot];
 	id = input_mt_get_value(mt, ABS_MT_TRACKING_ID);
-	if (id < 0 || input_mt_get_value(mt, ABS_MT_TOOL_TYPE) != tool_type)
+	if (id < 0 /*|| input_mt_get_value(mt, ABS_MT_TOOL_TYPE) != tool_type*/)
 		id = input_mt_new_trkid(dev);
 
 	input_event(dev, EV_ABS, ABS_MT_TRACKING_ID, id);
-	input_event(dev, EV_ABS, ABS_MT_TOOL_TYPE, tool_type);
+	//input_event(dev, EV_ABS, ABS_MT_TOOL_TYPE, tool_type);
 }
 EXPORT_SYMBOL(input_mt_report_slot_state);
 
@@ -115,10 +115,12 @@ EXPORT_SYMBOL(input_mt_report_slot_state);
 void input_mt_report_finger_count(struct input_dev *dev, int count)
 {
 	input_event(dev, EV_KEY, BTN_TOOL_FINGER, count == 1);
+	/*
 	input_event(dev, EV_KEY, BTN_TOOL_DOUBLETAP, count == 2);
 	input_event(dev, EV_KEY, BTN_TOOL_TRIPLETAP, count == 3);
 	input_event(dev, EV_KEY, BTN_TOOL_QUADTAP, count == 4);
 	input_event(dev, EV_KEY, BTN_TOOL_QUINTTAP, count == 5);
+	*/
 }
 EXPORT_SYMBOL(input_mt_report_finger_count);
 
