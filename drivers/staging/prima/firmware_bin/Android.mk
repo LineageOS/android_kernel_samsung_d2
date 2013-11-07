d60a5f2fe02830765e1c5408c324d6ca9f9aeb05
Dan Pasanen <dan.pasanen@gmail.com>
2013-11-07 09:56:58 -0600

    prima: add apex and express to build barrier for firmware
    Change-Id: I4d99c8534f06330e2457492ebe20c163c4818f0f

--- drivers/staging/prima/firmware_bin/Android.mk
+++ drivers/staging/prima/firmware_bin/Android.mk
@@ -1,7 +1,7 @@
-LOCAL_PATH:= $(call my-dir)
-
 ifeq ($(BOARD_VENDOR),samsung)
 ifeq ($(TARGET_BOARD_PLATFORM),msm8960)
+ifeq ($(filter apexqtmo expressatt,$(VARIENT_MODEL)),)
+LOCAL_PATH:= $(call my-dir)
 
 include $(CLEAR_VARS)
 
@@ -29,5 +29,6 @@ LOCAL_SRC_FILES    := WCNSS_qcom_wlan_nv.bin
 LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)/firmware/wlan/prima
 include $(BUILD_PREBUILT)
 
+endif
 endif
 endif
