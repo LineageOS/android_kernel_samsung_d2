bd61d094f07ad76bd5a0f8694625074199674803
Paul Keith <javelinanddart@bestas.gr>
2017-04-13 21:17:24 -0500

    cpufreq_limit/msm_thermal: Coordinate frequency limiting
    * This forces msm_thermal to use cpufreq_limit's
      frequency limiting mechanism, but also forces
      cpufreq_limit to back out of frequency changes
      if throttling is on, and if thermal_throttle()
      isn't sending the call to limit freqs
    * This should hopefully keep thermal throttling
      in much better shape than it was before
    * Also, cleanly handle unthrottling by setting
      the cpu frequency to what it would have
      been set to while throttling, rather than
      jumping straight to max cpu frequency
    
    Change-Id: If6f2fbe891023cf5fa97b7adbea6f28e4282a587
    Signed-off-by: Paul Keith <javelinanddart@bestas.gr>

---
+++ include/linux/cpufreq_limit.h
@@ -0,0 +1,19 @@
+/*
+ *  linux/include/linux/cpufreq_limit.h
+ *
+ *  Copyright (C) 2017 Paul Keith <javelinanddart@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ */
+#ifndef _LINUX_CPUFREQ_LIMIT_H
+#define _LINUX_CPUFREQ_LIMIT_H
+
+
+/* Thermal throttling function for thermal driver */
+extern void thermal_throttle(uint32_t max_freq, bool throttle);
+/* Thermal throttle removal function for thermal driver */
+extern void thermal_unthrottle(void);
+
+#endif /* _LINUX_CPUFREQ_LIMIT_H */
